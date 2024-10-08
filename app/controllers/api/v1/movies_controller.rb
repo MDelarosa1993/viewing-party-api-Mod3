e6c27ed7fb792f4a9d75b55425daf552faa2c8a3
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-10-07 23:40:19 -0400

    Added connection to movie details in the MovieService and wrote tests,routes, and contoller action

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -7,4 +7,10 @@ class Api::V1::MoviesController < ApplicationController
     end
       render json: movies
   end
+
+  def show
+    movie_id = params[:id]  
+    movie_details = MovieService.get_movie_details(movie_id)
+    render json: movie_details
+  end
 end
--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -54,5 +54,39 @@ RSpec.describe "Api::V1::Movies", type: :request do
       end
     end
   end
+
+  describe 'Get/movie_details' do 
+    it 'retrieves move details by id, add cast and reviews' do 
+      movie_id = 278
+      get "/api/v1/movies/#{movie_id}"
+
+      expect(response).to be_successful
+
+      movie_details = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+      
+      expect(movie_details[:title]).to be_a(String)
+      expect(movie_details[:release_year]).to be_a(Integer)
+      expect(movie_details[:vote_average]).to be_a(Float)
+      expect(movie_details[:runtime]).to be_a(Integer)
+      expect(movie_details[:summary]).to be_a(String)
+      expect(movie_details[:genres]).to be_an(Array)
+
+      expect(movie_details[:cast]).to be_an(Array)
+
+      movie_details[:cast].each do |member|
+        expect(member).to include(:character, :actor)
+        expect(member[:character]).to be_a(String)
+        expect(member[:actor]).to be_a(String)
+      end
+
+      expect(movie_details[:reviews]).to be_an(Array)
+      
+      movie_details[:reviews].each do |review|
+        expect(review).to include(:author, :review)
+        expect(review[:author]).to be_a(String)
+        expect(review[:review]).to be_a(String)
+      end
+    end
+  end
 end
 
