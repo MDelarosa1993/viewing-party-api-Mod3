d5c256c5659a456b8e6d591a009c333d87e53a9e
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-10-09 16:44:45 -0400

    Added test, controller action, route, for adding a user to an existing VP

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,18 +1,44 @@
 class Api::V1::ViewingPartiesController < ApplicationController
+  before_action :authorize_api_key, only: [:create, :update]
   def create
     viewing_party = ViewingParty.create_with_invitees(party_params, @current_user)
 
     if viewing_party
-      render json: ViewingPartySerializer.new(viewing_party).serializable_hash, status: :created
+      render json: ViewingPartySerializer.new(viewing_party)
     else
-      render json: { error: viewing_party.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: viewing_party.errors.full_messages }
     end
   end
+
+  def update
+    viewing_party = ViewingParty.find_by(id: params[:id])
+    new_invitee = User.find_by(id: params[:invitee_user_id])
+    
+    if viewing_party.nil?
+      return render json: { error: 'Viewing Party not found' }, status: :not_found
+    elsif new_invitee.nil?
+      return render json: { error: 'Invitee not found' }, status: :not_found
+    end
+
+    unless viewing_party.users.include?(new_invitee)
+      viewing_party.users << new_invitee
+    end
+
+    render json: ViewingPartySerializer.new(viewing_party).serializable_hash, status: :ok
+  end
+
+
+
 
   private
 
   def party_params
     params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, invitees: [])
+  end
+
+  def authorize_api_key
+    @current_user = User.find_by(api_key: params[:api_key])
+    render json: { error: 'Invalid API key' }, status: :unauthorized unless @current_user
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::API
-  before_action :authorize_api_key
-
-  private
-
-  def authorize_api_key
-    @current_user = User.find_by(api_key: params[:api_key])
-    render json: { error: 'Invalid API key' }, status: :unauthorized unless @current_user
-  end
+  
 end
