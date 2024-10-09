78a8d9b9e7a7d0c718faebbb91f8cf34b4d46a05
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-10-09 17:33:48 -0400

    Added association to users so i can display vp invited and hosted and modified the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,17 +3,19 @@ class UserSerializer
   attributes :name, :username, :api_key
 
   def self.format_user_list(users)
-    { data:
-        users.map do |user|
-          {
-            id: user.id.to_s,
-            type: "user",
-            attributes: {
-              name: user.name,
-              username: user.username
-            }
+    {
+      data: users.map do |user|
+        {
+          id: user.id.to_s,
+          type: "user",
+          attributes: {
+            name: user.name,
+            username: user.username,
+            viewing_parties_hosted: user.viewing_parties_hosted, # Add this method to the User model
+            viewing_parties_invited: user.viewing_parties # Directly gets invited parties
           }
-        end
+        }
+      end
     }
   end
 end
