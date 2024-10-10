0ac3aaf99d12618a215af5d77a9912f0cd3649ae
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-10-10 16:31:35 -0400

    Tested movie api with stubs

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Movies", type: :request do
   describe "Get/top_rated" do
     it "retrieves the most top_rated movies" do
+      json_response = File.read("spec/fixtures/top_rated_movies.json")
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated")
+        .with(query: { api_key: Rails.application.credentials.movie_api[:key] })
+        .to_return(status: 200, body: json_response)
       get "/api/v1/movies/top_rated"
       expect(response).to be_successful
 
@@ -29,8 +33,12 @@ RSpec.describe "Api::V1::Movies", type: :request do
 
   describe 'Get/search' do 
     it 'retrieves movie by name' do
-      query = "Jack Reacher"
-      get "/api/v1/movies/search", params: { query: query } 
+       json_response = File.read("spec/fixtures/movie_search.json")
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie")
+        .with(query: { query: "Jack Reacher", api_key: Rails.application.credentials.movie_api[:key] })
+        .to_return(status: 200, body: json_response)
+
+      get "/api/v1/movies/search", params: { query: "Jack Reacher" } 
       
       expect(response).to be_successful
 
@@ -56,8 +64,14 @@ RSpec.describe "Api::V1::Movies", type: :request do
   end
 
   describe 'Get/movie_details' do 
-    it 'retrieves move details by id, add cast and reviews' do 
+     it 'retrieves movie details by id, adds cast and reviews' do 
       movie_id = 278
+      json_response = File.read("spec/fixtures/movie_credits_reviews.json")  
+       stub_request(:get, "https://api.themoviedb.org/3/movie/#{movie_id}")
+        .with(query: { api_key: Rails.application.credentials.movie_api[:key], append_to_response: 'credits,reviews' })
+        .to_return(status: 200, body: json_response)
+     
+
       get "/api/v1/movies/#{movie_id}"
 
       expect(response).to be_successful
@@ -71,16 +85,16 @@ RSpec.describe "Api::V1::Movies", type: :request do
       expect(movie_details[:summary]).to be_a(String)
       expect(movie_details[:genres]).to be_an(Array)
 
+      # Validate the cast details
       expect(movie_details[:cast]).to be_an(Array)
-
       movie_details[:cast].each do |member|
         expect(member).to include(:character, :actor)
         expect(member[:character]).to be_a(String)
         expect(member[:actor]).to be_a(String)
       end
 
+      # Validate the reviews details
       expect(movie_details[:reviews]).to be_an(Array)
-      
       movie_details[:reviews].each do |review|
         expect(review).to include(:author, :review)
         expect(review[:author]).to be_a(String)
